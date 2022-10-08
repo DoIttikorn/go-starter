96c65b4ddbce0dbec511d9077c5b47444483fb1c
ittikorn chawkamud <aseaways1234@gmail.com>
2022-10-08 23:57:33 +0700

    variable and start condition

--- variable/01var.go
+++ variable/01var.go
@@ -24,6 +24,18 @@ const (
 
 // basic var run on runtime
 
+var (
+	product = "iphone"
+	quntity = 10
+	price   = 399.99
+	inStock = true
+)
+
+const (
+	PRODUCT = "iphone"
+	PRICE   = 456.90
+)
+
 func main() {
 	fmt.Println(TB)
 	msg := "Nikato" + " Alias"
@@ -33,5 +45,13 @@ func main() {
 	fmt.Println("msg:", msg)
 	fmt.Println("age:", age)
 	fmt.Println("price:", price)
-	fmt.Println("check:", check)
+	fmt.Print("check:\n\n", check)
+
+	fmt.Println("product:", product)
+	fmt.Println("quntity:", quntity)
+	fmt.Println("price:", price)
+	fmt.Print("inStock:\n\n", inStock)
+
+	fmt.Println("PRODUCT:", PRODUCT)
+	fmt.Println("PRICE:", PRICE)
 }
