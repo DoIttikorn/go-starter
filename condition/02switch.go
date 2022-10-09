a1a95e655931c11c93e725f725572d634552924a
ittikorn chawkamud <aseaways1234@gmail.com>
2022-10-09 20:19:29 +0700

    learn: switch

--- condition/02switch.go
+++ condition/02switch.go
@@ -1,9 +1,11 @@
 package main
 
 func main() {
-	today := "Saturday"
+
 	// fallthrough คือการทำให้ case ถัดไปทำงานต่อไป 1 ครั้ง
-	switch today {
+	// ถ้าไม่มี fallthrough จะหยุดทำงานทันที
+	// short statement ใน case จะมี scope ใน case เท่านั้น
+	switch today := "Saturday"; today {
 	case "Monday":
 		println("วันจันทร์")
 	case "Tuesday":
@@ -14,12 +16,33 @@ func main() {
 		println("วันพฤหัสบดี")
 	case "Friday":
 		println("วันศุกร์")
-	case "Saturday":
-		println("วันเสาร์")
+	case "Saturday", "Sunday":
+		println("วันหยุด")
 		fallthrough
-	case "Sunday":
-		println("วันอาทิตย์")
 	default:
 		println("ไม่มีวันนี้")
+	}
+
+	num := 1
+	switch {
+	case num < 0:
+		println("nagative number")
+	case num > 0:
+		println("positive number")
+	default:
+		println("zero")
+	}
+	rating := 8.4
+	switch {
+	case rating < 5.0:
+		println("bad")
+	case rating >= 5.0 && rating < 7.0:
+		println("normal")
+	case rating >= 7.0 && rating < 9.0:
+		println("good")
+	case rating >= 9.0:
+		println("excellent")
+	default:
+		println("unknown")
 	}
 }
