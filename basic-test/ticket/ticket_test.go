c3eecc63b3639197c5d9cdfaf3d6dc4806db1a51
ittikorn chawkamud <aseaways1234@gmail.com>
2022-11-27 19:44:12 +0700

    test table

--- basic-test/ticket/ticket_test.go
+++ basic-test/ticket/ticket_test.go
@@ -4,87 +4,28 @@ import "testing"
 
 // เป็นการ test โดยอาศัย Boundary values
 func TestTicketPrice(t *testing.T) {
-	t.Run("tiker $0 when age is 0", func(t *testing.T) {
-		// Arrange
-		want := 0.0
-		const age uint = 0
-		// Act
-		result := Price(age)
-		// Assert
-		if result != want {
-			t.Errorf("Price(0) = %f; want %f", result, want)
-		}
-	})
+	tests := []struct {
+		name string
+		age  int
+		want float64
+	}{
+		{name: "Free Ticket when age is 0", age: 0, want: 0.0},
+		{name: "Free Ticket when age under 3", age: 3, want: 0.0},
+		{name: "Ticket $15 when age at 4 year old", age: 4, want: 15.0},
+		{name: "Ticket $15 when age is 15", age: 15, want: 15.0},
+		{name: "Ticket $30 when age over 15", age: 16, want: 30.0},
+		{name: "Ticket $30 when age is 50", age: 50, want: 30.0},
+		{name: "Ticket $5 when age over 50", age: 51, want: 5.0},
+	}
 
-	t.Run("Ticket $0 when age is 3", func(t *testing.T) {
-		// Arrange
-		want := 0.0
-		const age uint = 3
-		// Act
-		result := Price(age)
-		// Assert
-		if result != want {
-			t.Errorf("Price(3) = %f; want %f", result, want)
-		}
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Price(tt.age)
 
-	t.Run("Ticket $15 when age is 4", func(t *testing.T) {
-		// Arrange
-		want := 15.0
-		const age uint = 4
-		// Act
-		result := Price(age)
-		// Assert
-		if result != want {
-			t.Errorf("Price(4) = %f; want %f", result, want)
-		}
-	})
+			if got != tt.want {
+				t.Errorf("Price(%d) = %f, want %f", tt.age, got, tt.want)
+			}
 
-	t.Run("Ticket $15 when age is 15", func(t *testing.T) {
-		// Arrange
-		want := 15.0
-		const age uint = 15
-		// Act
-		result := Price(age)
-		// Assert
-		if result != want {
-			t.Errorf("Price(15) = %f; want %f", result, want)
-		}
-	})
-
-	t.Run("Ticket $30 when age over 15", func(t *testing.T) {
-		// Arrange
-		want := 30.0
-		const age uint = 16
-		// Act
-		result := Price(age)
-		// Assert
-		if result != want {
-			t.Errorf("Price(16) = %f; want %f", result, want)
-		}
-	})
-
-	t.Run("Ticket $30 when age over 30", func(t *testing.T) {
-		// Arrange
-		want := 30.0
-		var age uint = 30
-		// Act
-		result := Price(age)
-		// Assert
-		if result != want {
-			t.Errorf("Price(30) = %f; want %f", result, want)
-		}
-	})
-
-	t.Run("Ticket $5 when age over 50", func(t *testing.T) {
-		// Arrange
-		want := 5.0
-		var age uint = 51
-		// Act
-		result := Price(age)
-		// Assert
-		if result != want {
-			t.Errorf("Price(51) = %f; want %f", result, want)
-		}
-	})
+		})
+	}
 }
