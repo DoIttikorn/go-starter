68821b93819e320bc1ed5fa1b7f7fcbbcf9632f6
ittikorn chawkamud <aseaways1234@gmail.com>
2022-12-05 02:16:57 +0700

    learn: struct medthod not pointer

--- basic/pointer/workshop.go
+++ basic/pointer/workshop.go
@@ -15,6 +15,11 @@ func (m *movie) addVote(rating float64) {
 	m.votes = append(m.votes, rating)
 }
 
+func (m movie) addVoteNotPointer(rating float64) {
+	m.votes = append(m.votes, rating)
+	fmt.Printf("m: %#+v\n", m)
+}
+
 func main() {
 
 	eg := &movie{
@@ -28,4 +33,16 @@ func main() {
 
 	eg.addVote(8)
 	fmt.Printf("eg.votes: %#+v\n", eg.votes)
+
+	ep := movie{
+		title:       "The Matrix",
+		year:        1999,
+		rating:      8.7,
+		votes:       []float64{8.7, 8.8, 8.9},
+		genres:      []string{"Action", "Sci-Fi"},
+		isSuperHero: false,
+	}
+
+	ep.addVoteNotPointer(9999)
+	fmt.Printf("ep : %#+v\n", ep)
 }
